4447e4a08df85553e371f0b968698ff4dd9daa64
büsra <buesra.kablan@student.htldornbirn.at>
2025-04-02 08:25:48 +0200

    finished with games 03 Objekts

--- src/at/bus/games/firstgame/Objekts.java
+++ src/at/bus/games/firstgame/Objekts.java
@@ -3,87 +3,110 @@ package at.bus.games.firstgame;
 import org.newdawn.slick.*;
 
 public class Objekts extends BasicGame {
-        private float x;
-        private float y;
-        private float speedX;
-        private float speedY;
-        private float x1;
-        private float y1;
-        private float speedX1;
-        private float speedY1;
-        private float x2;
-        private float y2;
-        private float speed2;
-        public void Rectangles(String title) {
+    private float x;
+    private float y;
+    private float speedX;
+    private float speedY;
+    private float x1;
+    private float y1;
+    private float speedX1;
+    private float speedY1;
+    private float x2;
+    private float y2;
+    private float speedX2;
+    private float speedY2;
 
-        }
+    private boolean isGoingDown = false;
+
+    public void Rectangles(String title) {
+
+    }
 
     public Objekts(String title) {
         super(title);
     }
 
     @Override
-        public void init(GameContainer gameContainer) throws SlickException {
+    public void init(GameContainer gameContainer) throws SlickException {
+        this.x = 100;
+        this.y = 100;
+        this.speedX = 5.0f;
+        this.speedY = 5.0f;
+
+        this.x1 = 400;
+        this.y1 = 100;
+        this.speedX1 = 3.0f;
+        this.speedY1 = 3.0f;
+
+        this.x2 = 50;
+        this.y2 = 410;
+        this.speedX2 = 3.0f;
+        this.speedY2 = 3.0f;
+
+
+    }
+
+    @Override
+    public void update(GameContainer gameContainer, int delta) throws SlickException {
+
+        if (this.x < 600 && this.y == 100) {
+            this.x += (float) delta / this.speedX;
+        } else if (this.x >= 600 && this.y < 400) {
+            this.y += (float) delta / this.speedY;
+        } else if (this.y >= 400 && this.x > 100) {
+            this.x -= (float) delta / this.speedX;
+        } else if (this.x <= 100 && this.y > 100) {
+            this.y -= (float) delta / this.speedY;
+        }
+
+
+        if (this.x <= 100 && this.y <= 100) {
             this.x = 100;
-            this.y= 100;
-            this.speedX=5.0f;
-            this.speedY = 5.0f;
+            this.y = 100;
+        }
 
+
+        this.x1 -= (float) delta / this.speedX1;
+        if (this.x1 <= 100) {
             this.x1 = 400;
-            this.y1=100;
-            this.speedX1=3.0f;
-            this.speedY1 = 3.0f;
-
         }
 
-        @Override
-        public void update(GameContainer gameContainer, int delta) throws SlickException {
-
-            if (this.x < 600 && this.y == 100) {
-                this.x += (float) delta / this.speedX;
+        if (isGoingDown) {
+            this.y2++;
+            //this.y2 += (float) delta / this.speedX2;
+            if(this.y2 >=410) {
+                isGoingDown=false;
             }
-            else if (this.x >= 600 && this.y < 400) {
-                this.y += (float) delta / this.speedY;
-            }
-            else if (this.y >= 400 && this.x > 100) {
-                this.x -= (float) delta / this.speedX;
-            }
-            else if (this.x <= 100 && this.y > 100) {
-                this.y -= (float) delta / this.speedY;
-            }
+        } else {
+            this.y2--;
+            //this.y2 -= (float) delta / this.speedX2;
+            if(this.y2 <=100) {
 
-
-            if (this.x <= 100 && this.y <= 100) {
-                this.x = 100;
-                this.y = 100;
-            }
-
-
-
-            this.x1 -= (float) delta / this.speedX1;
-            if (this.x1 <= 100) {
-                this.x1 = 400;
+                isGoingDown=true;
             }
         }
-
-        @Override
-        public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
-            graphics.drawRect(this.x,this.y,100,100);
-            graphics.drawString("Hello you!",50,50);
-            graphics.drawOval(this.x1,this.y1,50,25);
-            graphics.drawOval(100,this.y,100,100);
-        }
+        System.out.println("y2" + y2    );
 
 
+    }
 
-        public static void main(String[] argv) {
-            try {
-                AppGameContainer container = new AppGameContainer(new at.bus.games.firstgame.Objekts("Objekts"));
-                container.setDisplayMode(800,600,false);
-                container.start();
-            } catch (SlickException e) {
-                e.printStackTrace();
-            }
+    @Override
+    public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
+        graphics.drawRect(this.x, this.y, 100, 100);
+        graphics.drawString("Hello you!", 50, 50);
+        graphics.drawOval(this.x1, this.y1, 50, 25);
+        graphics.drawOval(this.x2, this.y2, 100, 100);
+    }
+
+
+    public static void main(String[] argv) {
+        try {
+            AppGameContainer container = new AppGameContainer(new at.bus.games.firstgame.Objekts("Objekts"));
+            container.setDisplayMode(800, 600, false);
+            container.start();
+        } catch (SlickException e) {
+            e.printStackTrace();
         }
     }
+}
 
