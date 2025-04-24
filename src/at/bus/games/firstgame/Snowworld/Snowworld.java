2e0c2abb14f7c589cb31a0228d8a3cdd8d5d867c
büsra <buesra.kablan@student.htldornbirn.at>
2025-04-24 09:12:59 +0200

    beginn with games snowworld

---
+++ src/at/bus/games/firstgame/Snowworld/Snowworld.java
@@ -0,0 +1,27 @@
+package at.bus.games.firstgame.Snowworld;
+
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+public class Snowworld extends BasicGame {
+    public Snowworld(String title) {
+        super(title);
+    }
+
+    @Override
+    public void init(GameContainer gameContainer) throws SlickException {
+
+    }
+
+    @Override
+    public void update(GameContainer gameContainer, int i) throws SlickException {
+
+    }
+
+    @Override
+    public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
+
+    }
+}
